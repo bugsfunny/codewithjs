5a8d077031d80dcef18276129a9bb3fd036aa4fd
Bugsfunny <jsdenize@edenschool.fr>
2023-02-01 15:31:13 +0100

    Add mongo connection

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 
-const connectionString = 'mongodb://mongo:27017/codewithjs';
-
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DATABASE_URI)
