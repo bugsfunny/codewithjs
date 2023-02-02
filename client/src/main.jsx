4fe30b1633363e17daaaf290d12d4259de5fdfca
Bugsfunny <jsdenize@edenschool.fr>
2023-02-02 17:51:47 +0100

    Add some components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+    </Route>
+  )
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode >,
 )
